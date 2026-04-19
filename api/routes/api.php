b7de2dd70bf75842aaef2a70a33558191cc59788
Adam Salehuddin <adamsalehuddin91@gmail.com>
2026-04-20 00:32:09 +0800

    Add /api/health endpoint

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\WisdomController;
 use App\Http\Middleware\VerifyN8NToken;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/health', fn () => response()->json(['status' => 'ok']));
+
 Route::prefix('v1')->group(function () {
     // Public
     Route::get('/wisdom/random', [WisdomController::class, 'random']);
